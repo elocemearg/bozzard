3ab38258ef3c2cea32f195a65f55293886400977
Graeme Cole <graeme@greem.co.uk>
2019-11-17 16:13:04 +0000

    About: make play button move to next page
    Also save some code space.

--- sysinfo.ino
+++ sysinfo.ino
@@ -10,10 +10,11 @@ byte sysinfo_current_page = 0;
 
 void
 sysinfo_init(void *dummy) {
-    /* Press any QM button to exit */
+    /* Press red reset button to exit */
     boz_set_event_handler_qm_reset(sysinfo_exit);
-    boz_set_event_handler_qm_yellow(sysinfo_exit);
-    boz_set_event_handler_qm_play(sysinfo_exit);
+
+    /* Press play to cycle through pages */
+    boz_set_event_handler_qm_play(sysinfo_next_page);
 
     /* Turn the knob to switch pages */
     boz_set_event_handler_qm_rotary(sysinfo_turn_page);
@@ -26,36 +27,47 @@ sysinfo_exit(void *cookie) {
     boz_app_exit(0);
 }
 
+/* There are only two pages: the one showing the version information, and the
+   one showing the compilation date. */
 void
-sysinfo_turn_page(void *cookie, int clockwise) {
-    if (sysinfo_current_page != clockwise) {
-        sysinfo_refresh((byte) clockwise);
+sysinfo_turn_page(void *cookie, int forwards) {
+    if (sysinfo_current_page != forwards) {
+        sysinfo_refresh((byte) forwards);
     }
 }
 
 void
+sysinfo_next_page(void *cookie) {
+    sysinfo_refresh(!sysinfo_current_page);
+}
+
+void
 sysinfo_refresh(byte page) {
+    const char *bottom_line;
+
     sysinfo_current_page = page;
 
     boz_display_clear();
+
     if (page == 0) {
         long version = boz_get_version();
 
         /* Write Bozzard followed by the version number */
         boz_display_write_string_P(s_sysinfo_boz_v);
-        for (int i = 0; i < 3; ++i) {
-            boz_display_write_long((version >> (24 - i * 8)) & 0xff, 0, NULL);
+        for (byte i = 0; i < 3; ++i) {
+            boz_display_write_long((version >> 24) & 0xff, 0, NULL);
             if (i < 2)
                 boz_display_write_char('.');
+            version <<= 8;
         }
 
         /* Show the copyright information */
-        boz_display_set_cursor(1, 0);
-        boz_display_write_string_P(s_sysinfo_copyright);
+        bottom_line = s_sysinfo_copyright;
     }
     else {
         boz_display_write_string_P(s_sysinfo_compiled_on);
-        boz_display_set_cursor(1, 0);
-        boz_display_write_string_P(s_sysinfo_compilation_date);
+        bottom_line = s_sysinfo_compilation_date;
     }
+    boz_display_set_cursor(1, 0);
+    boz_display_write_string_P(bottom_line);
 }
